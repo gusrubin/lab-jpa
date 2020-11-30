54453fd4553d84d56f1d024ddccb11c522cccb2d
Gustavo Rubin <gustavo.rubin@benfatto.net.br>
2020-11-30 16:02:04 -0300

    Integration Tests

--- src/test/java/com/gusrubin/lab/jpa/onetomany/CartServiceTests.java
+++ src/test/java/com/gusrubin/lab/jpa/onetomany/CartServiceTests.java
@@ -3,7 +3,7 @@ package com.gusrubin.lab.jpa.onetomany;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 
-import java.util.UUID;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,17 +14,11 @@ import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.gusrubin.lab.jpa.api.onetomany.dto.CartDTO;
-import com.gusrubin.lab.jpa.api.simpleentity.dto.CustomerDTO;
 import com.gusrubin.lab.jpa.onetomany.builder.CartBuilder;
 import com.gusrubin.lab.jpa.repository.onetomany.CartRepository;
 import com.gusrubin.lab.jpa.repository.onetomany.entity.Cart;
-import com.gusrubin.lab.jpa.repository.simpleentity.CustomerRepository;
-import com.gusrubin.lab.jpa.repository.simpleentity.entity.CustomerEntity;
 import com.gusrubin.lab.jpa.service.onetomany.CartService;
 import com.gusrubin.lab.jpa.service.onetomany.mapper.CartMapper;
-import com.gusrubin.lab.jpa.service.simpleentity.CustomerService;
-import com.gusrubin.lab.jpa.service.simpleentity.mapper.CustomerMapper;
-import com.gusrubin.lab.jpa.simpleentity.builder.CustomerBuilder;
 
 @ExtendWith(MockitoExtension.class)
 class CartServiceTests {
@@ -50,6 +44,30 @@ class CartServiceTests {
 		verify(repository).save(Mockito.any());
 		
 		assertEquals(expectedEntity.getId(), dto.getId());
+	}
+	
+	@Test
+	void shouldSaveCartWithOneItemSuccessfully() {
+		Cart expectedEntity = CartBuilder.buildCartWithOneItem();
+		
+		Mockito.when(repository.save(Mockito.any())).thenReturn(expectedEntity);
+		
+		CartDTO dto = serviceSpy.save();
+		
+		verify(repository).save(Mockito.any());
+		
+		assertEquals(expectedEntity.getId(), dto.getId());
+	}	
+	
+	@Test
+	void shouldDeleteCartSucessfully() {
+		Optional<Cart> expectedEntity = Optional.of(CartBuilder.buildCartWithoutItems());
+		
+		Mockito.when(repository.findById(Mockito.any())).thenReturn(expectedEntity);
+		
+		serviceSpy.delete(expectedEntity.get().getId());
+		
+		verify(repository).delete(Mockito.any());		
 	}
 
 }
